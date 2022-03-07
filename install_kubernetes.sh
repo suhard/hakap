c930b75af593d46a12fd8742fca764788ec5c2ba
suhard <63784117+suhard@users.noreply.github.com>
2022-03-07 10:08:17 +0100

    Create install_kubernetes.sh

---
+++ install_kubernetes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Installing Kubernetes
+KUBERNETES_INSTALLED=$(which kubeadm)
+if [ "$KUBERNETES_INSTALLED" = "" ]
+then
+        curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+        touch /etc/apt/sources.list.d/kubernetes.list
+        chmod 666 /etc/apt/sources.list.d/kubernetes.list
+        echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+        apt-get update
+        apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+fi
+
+#Disabling swap for Kubernetes
+sysctl net.bridge.bridge-nf-call-iptables=1 > /dev/null
+swapoff -a
